952e94073639c236c8ee53cdfed2370d699b816c
MaksymPysanyi <maksbanda@gmail.com>
2024-03-22 17:27:24 +0200

    add power operation

--- Calculator.py
+++ Calculator.py
@@ -1,2 +1,19 @@
-class calculator:
+import unittest
+class Calculator:
+    def power (self, a, b):
+        return a ** b
     
+class TestCalculator(unittest.TestCase):
+    def setUp(self):
+        self.calculator = calculator()
+
+    def test_power(self):
+        calculator = Calculator()
+        self.assertEqual(calculator.power(5, 3), 125)
+
+if __name__ == "__main__":
+    unittest.main()
+
+calculator = Calculator()
+result = calculator.power(5, 3)
+print("Результат", result)
