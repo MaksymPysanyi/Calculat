f2ab76039ec7d0a5b7a3fed127a4fbd124369b68
MaksymPysanyi <maksbanda@gmail.com>
2024-03-22 17:55:42 +0200

    add gsd operation

--- Calculator.py
+++ Calculator.py
@@ -3,13 +3,22 @@ class Calculator:
     def power (self, a, b):
         return a ** b
     
+    def gsd(self, a, b):
+        while b:
+            a, b = b, a % b
+        return a
+    
 class TestCalculator(unittest.TestCase):
     def setUp(self):
-        self.calculator = calculator()
+        self.calculator = Calculator()
 
     def test_power(self):
         calculator = Calculator()
         self.assertEqual(calculator.power(5, 3), 125)
+
+    def test_gsd(self):
+        calculator = Calculator()
+        self.assertEqual(calculator.gsd(12, 18), 6)
 
 if __name__ == "__main__":
     unittest.main()
