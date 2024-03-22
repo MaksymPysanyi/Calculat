fb48370f6373d25f01110a17fe9bf3c884546187
MaksymPysanyi <maksbanda@gmail.com>
2024-03-22 17:08:41 +0200

    add subtraction operation

--- Calculator.py
+++ Calculator.py
@@ -1,20 +1,29 @@
 import unittest
-class calculator:
+class Calculator:
     def add(self, a, b):
         return a + b
+    
+    def subtract(self, a, b):
+        return a - b
 
 class TestCalculator(unittest.TestCase):
     def setUp(self):
-        self.cal = calculator()
+        self.calculator = Calculator()
     
     def test_add(self):
         a, b = 5, 3
-        result = self.cal.add(a, b)
+        result = self.calculator.add(a, b)
         self.assertEqual(result, 8, "Sum of 5 and 3 should be 8")
+    
+    def test_subtract(self):
+        a, b = 5, 3
+        result=self.calculator.subtract(a,b)
+        self.assertEqual(result, 2, "Subtract of 5 and 3 sholud be 2")
+
 
 if __name__ == "__main__":
     unittest.main()
 
-cal = calculator()
-result = cal.add(5, 3)
+calculator = Calculator()
+result = calculator.add(5, 3)
 print("Результат", result)
